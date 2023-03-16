c26a4699013563695213da9ea2a98227183a8863
Nico Pom <niconp7@yahoo.fr>
2023-03-16 18:45:09 +0100

    commenting code

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ const btn = document.querySelector(".btn");
 const span = document.querySelector("span");
 
 btn.addEventListener("mouseenter", function (e) {
+  // get the position of the button
   var parentOffset = this.getBoundingClientRect(),
+    // get the position of the mouse relative to the button
     relX = e.pageX - parentOffset.left,
     relY = e.pageY - parentOffset.top;
 
   span.style.top = relY + "px";
   span.style.left = relX + "px";
 });
+
 btn.addEventListener("mouseout", function (e) {
   var parentOffset = this.getBoundingClientRect(),
     relX = e.pageX - parentOffset.left,
