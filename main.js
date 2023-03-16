3277f9392744b8bd50d63379c373e034e2246b74
Nico Pom <niconp7@yahoo.fr>
2023-03-16 18:38:01 +0100

    init

---
+++ main.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector(".btn");
+const span = document.querySelector("span");
+
+btn.addEventListener("mouseenter", function (e) {
+  var parentOffset = this.getBoundingClientRect(),
+    relX = e.pageX - parentOffset.left,
+    relY = e.pageY - parentOffset.top;
+
+  span.style.top = relY + "px";
+  span.style.left = relX + "px";
+});
+btn.addEventListener("mouseout", function (e) {
+  var parentOffset = this.getBoundingClientRect(),
+    relX = e.pageX - parentOffset.left,
+    relY = e.pageY - parentOffset.top;
+
+  span.style.top = relY + "px";
+  span.style.left = relX + "px";
+});
